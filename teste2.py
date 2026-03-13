b4ef4f74e002866a6c514e1112ace459259d6658
Matheus Costa <stsycosta@gmail.com>
2026-03-12 21:17:24 -0300

    Delete teste2.py

--- teste2.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Primeiro número inteiro: ")); 
-num2 = int(input("Segundo número inteiro: ")); 
-
-divisao = num1 / num2
-adicao = num1 + num2
-subtracao = num1 - num2
-multiplicacao = num1 * num2
-
-print("Soma:", adicao, "Subtração:", subtracao, "Divisão:", divisao, "Multiplicação:", multiplicacao); 
