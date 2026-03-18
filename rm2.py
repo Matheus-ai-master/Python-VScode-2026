cde66af0fc2c5283bc1741ed4fff7fd6dc0d2be1
Matheus Costa <stsycosta@gmail.com>
2026-03-17 21:49:19 -0300

    Add files via upload

---
+++ rm2.py
@@ -0,0 +1,29 @@
+num = int(input("RM do aluno: ")); 
+
+soma = 0
+
+un = num % 10
+soma = soma + un 
+num = num // 10
+
+
+un = num % 10
+soma = soma + un 
+num = num // 10
+
+
+un = num % 10
+soma = soma + un 
+num = num // 10
+
+
+un = num % 10
+soma = soma + un 
+num = num // 10
+
+
+un = num % 10
+soma = soma + un 
+num = num // 10
+
+print("Soma dos algarismos do RM:", soma); 
